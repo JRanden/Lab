19c9f360e683d358e4306aa863ef8fe3c8f2e51f
Jonathan Randen <jonathanranden@gmail.com>
2026-05-18 15:26:35 +0200

    La til error handling

---
+++ Python/pingChecker/checker.py
@@ -0,0 +1,19 @@
+import subprocess
+
+ipAddresses = ["127.0.0.1", "192.168.116.2", "10.0.0.3", "google.com", "vg.no"]
+onlineHosts = []
+offlineHosts = []
+
+def checkPing(): 
+    for ip in ipAddresses:
+        try:
+            subprocess.run(["ping", "-c", "2", ip], capture_output=True, check=True, timeout=4)
+            onlineHosts.append(ip)
+            print(f"{ip} er online")
+        except (subprocess.TimeoutExpired, subprocess.CalledProcessError):
+            offlineHosts.append(ip)
+            print(f"{ip} er offline")
+
+checkPing()
+print("Online:", onlineHosts)
+print("Offline:", offlineHosts)
